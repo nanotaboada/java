0ed65c207e9ac2967d46f486fc8bc8086fa40880
Nano Taboada <nanotaboada@msn.com>
2014-06-20 01:44:41 -0300

    Adding TestNG sample project.

---
+++ java.samples.testng/src/ar/com/nanotaboada/java/samples/testng/ProgramTest.java
@@ -0,0 +1,55 @@
+/** MIT-LICENSE
+ * Copyright (c) 2014 Nano Taboada, http://openid.nanotaboada.com.ar
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ **/
+
+package ar.com.nanotaboada.java.samples.testng;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class ProgramTest
+{
+  @Test
+  public void getPangram_whenInvoked_thenReturnValueIsNotNull()
+  {
+    // Arrange
+    Program program = new Program();
+
+    // Act
+    final String value = program.getPangram();
+
+    // Assert
+    Assert.assertNotNull(value);
+  }
+
+  @Test
+  public void getPangram_whenInvoked_thenReturnsExpectedValue()
+  {
+    // Arrange
+    Program program = new Program();
+    final String expected = "The quick brown fox jumps over the lazy dog.";
+
+    // Act
+    final String actual = program.getPangram();
+
+    // Assert
+    Assert.assertEquals(expected, actual);
+  }
+}
